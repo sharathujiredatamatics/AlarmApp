c34334ff62067c31bb6a1ac31470b99dbcd640d8
sharathujiredatamatics <sharath.ujire@digital.datamatics.com>
2021-03-24 13:57:34 +0530

    Changed Time Format

--- AlarmApp/AppDelegate.swift
+++ AlarmApp/AppDelegate.swift
@@ -14,7 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         print(urls[urls.count-1] as URL)
